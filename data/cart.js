a64fecdf98cfb5fd79114bcc1e0858cb27764fb8
Jeff Yankson <jjyankson19@gmail.com>
2026-06-07 14:42:49 +0000

    Fix addToCart bug and save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,26 @@
-export let cart = [
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    qty: 2,
-  },
-  {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    qty: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      qty: 2,
+    },
+    {
+      id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      qty: 1,
+    },
+  ];
+}
+
+/**
+ *
+ * @param {*} localStorage
+ * save item in localstorage
+ */
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   // Deduplicate products.
@@ -22,20 +35,23 @@ export function addToCart(productId) {
     matchingItem.qty += 1;
   } else {
     cart.push({
-      productId,
+      id: productId,
       qty: 1,
     });
   }
+  console.log(cart);
+  saveToStorage();
 }
 
-export function removeFromCart(productId) {
+// remove items from cart.
+export function removeItemFromCart(productId) {
   const newCart = [];
 
   cart.forEach((cartItem) => {
-    if (cartItem.id !== productId) {
+    if (productId !== cartItem.id) {
       newCart.push(cartItem);
     }
   });
-
   cart = newCart;
+  saveToStorage();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeItemFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-  console.log(matchingProduct);
+
 
   cartSummaryHTML += `
     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -39,7 +39,7 @@ cart.forEach((cartItem) => {
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary js-delete-link" 
+                <span class="delete-quantity-link link-primary js-delete-quantity-link"
                 data-product-id=${matchingProduct.id}>
                 Delete
                 </span>
@@ -90,12 +90,10 @@ cart.forEach((cartItem) => {
     `;
   document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 });
-
-// Make delete links interactive.
-document.querySelectorAll(".js-delete-link").forEach((link) => {
-  link.addEventListener("click", () => {
+document.querySelectorAll(".js-delete-quantity-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
     const { productId } = link.dataset;
-    removeFromCart(productId);
+    removeItemFromCart(productId);
 
     const container = document.querySelector(
       `.js-cart-item-container-${productId}`,
