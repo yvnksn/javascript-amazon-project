e8a675f27b8823afa839798167d917e1da7751c8
Jeff Yankson <jjyankson19@gmail.com>
2026-06-14 17:59:50 +0000

    remove loadCart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,6 @@ import renderOrderSummary from './checkout/orderSummary.js';
 import renderPaymentSummary from './checkout/paymentSummary.js';
 // import "../data/backend-practice.js";
 import { loadProductsFromFetch } from '../data/products.js';
-import { loadCart } from '../data/cart.js';
 
 async function loadPage() {
   console.log('async load page');
